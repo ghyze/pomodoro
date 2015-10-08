d592076451ad9ab9ab968a17ee96bd2f70291335
gijs <govervliet@gmail.com>
2015-10-08 21:54:18 +0200

    Added unittests

--- src/main/java/nl/ghyze/pomodoro/controller/PomodoroStateMachine.java
+++ src/main/java/nl/ghyze/pomodoro/controller/PomodoroStateMachine.java
@@ -7,96 +7,93 @@ import nl.ghyze.pomodoro.view.systemtray.SystemTrayManager;
 
 public class PomodoroStateMachine {
 
-    private Pomodoro current;
-    private Settings settings;
-    private int pomosDone = 0;
-    private SystemTrayManager systemTrayManager;
+	private Pomodoro current;
+	private Settings settings;
+	private int pomosDone = 0;
+	private SystemTrayManager systemTrayManager;
 
-    public PomodoroStateMachine(Settings settings) {
-	this.settings = settings;
-	current = new Pomodoro(0, Type.WAIT);
-    }
-    
-    public void setSystemTrayManager(SystemTrayManager systemTrayManager){
-	this.systemTrayManager = systemTrayManager;
-    }
+	public PomodoroStateMachine(Settings settings) {
+		this.settings = settings;
+		current = new Pomodoro(0, Type.WAIT);
+	}
 
-    public Pomodoro getCurrent() {
-	return current;
-    }
-    
-    public Pomodoro.Type getCurrentType(){
-	return current.getType();
-    }
+	public void setSystemTrayManager(SystemTrayManager systemTrayManager) {
+		this.systemTrayManager = systemTrayManager;
+	}
 
-    public boolean shouldChangeState() {
-	return (current.getType() != Type.WAIT && current.isDone());
-    }
+	public Pomodoro getCurrent() {
+		return current;
+	}
 
-    public void handleAction(int choice) {
-	if (current.getType() == Type.POMO) {
-	    System.out.println("Finished choice: " + choice);
-	    if (choice == 0) {
-		pomosDone++;
-	    }
-	    Pomodoro next = getNext(Type.POMO);
-	    current = next;
-	    updateCurrent();
+	public Pomodoro.Type getCurrentType() {
+		return current.getType();
+	}
 
-	} else if (current.getType() == Type.BREAK) {
-	    System.out.println("New choice: " + choice);
-	    if (choice == 0) {
-		startPomo();
-	    } else {
+	public boolean shouldChangeState() {
+		return (current.getType() != Type.WAIT && current.isDone());
+	}
+
+	public void handleAction(int choice) {
+		if (current.getType() == Type.POMO) {
+			if (choice == 0) {
+				pomosDone++;
+			}
+			Pomodoro next = getNext(Type.POMO);
+			current = next;
+			updateCurrent();
+
+		} else if (current.getType() == Type.BREAK) {
+			if (choice == 0) {
+				startPomo();
+			} else {
+				startWait();
+			}
+		}
+
+	}
+
+	private Pomodoro getNext(Type type) {
+		if (type == Type.POMO) {
+			if (pomosDone < settings.getPomosBeforeLongBreak()) {
+				String message = "Well done: Short break";
+				return new Pomodoro(settings.getShortBreakMinutes(), Type.BREAK);
+			} else {
+				pomosDone = 0;
+				String message = "Well done: Long break";
+				return new Pomodoro(settings.getLongBreakMinutes(), Type.BREAK);
+			}
+		} else if (type == Type.BREAK) {
+			String message = "Waiting to start next Pomodoro";
+			showMessage(message);
+			return new Pomodoro(0, Type.WAIT);
+		}
+		return null;
+	}
+
+	public void stopCurrent() {
 		startWait();
-	    }
 	}
 
-    }
-    
-    protected Pomodoro getNext(Type type) {
-	if (type == Type.POMO) {
-	    if (pomosDone < settings.getPomosBeforeLongBreak()) {
-		String message = "Well done: Short break";
-		return new Pomodoro(settings.getShortBreakMinutes(),
-			Type.BREAK);
-	    } else {
-		pomosDone = 0;
-		String message = "Well done: Long break";
-		return new Pomodoro(settings.getLongBreakMinutes(), Type.BREAK);
-	    }
-	} else if (type == Type.BREAK) {
-	    String message = "Waiting to start next Pomodoro";
-	    showMessage(message);
-	    return new Pomodoro(0, Type.WAIT);
+	public void startWait() {
+		current = new Pomodoro(0, Type.WAIT);
+		updateCurrent();
 	}
-	return null;
-    }
 
-    public void stopCurrent() {
-	startWait();
-    }
-
-    public void startWait() {
-	current = new Pomodoro(0, Type.WAIT);
-	updateCurrent();
-    }
-
-    public void startPomo() {
-	current = new Pomodoro(25, Type.POMO);
-	updateCurrent();
-	String message = "Starting Pomodoro number " + (pomosDone + 1);
-	showMessage(message);
-    }
-
-    private void showMessage(String message) {
-	if (systemTrayManager != null){
-	    systemTrayManager.message(message);
+	public void startPomo() {
+		current = new Pomodoro(settings.getPomoMinutes(), Type.POMO);
+		updateCurrent();
+		String message = "Starting Pomodoro number " + (pomosDone + 1);
+		showMessage(message);
 	}
-    }
 
-    public void updateCurrent() {
-	current.setPomosDone(pomosDone);
-	current.setMaxPomosDone(settings.getPomosBeforeLongBreak());
-    }
+	private void showMessage(String message) {
+		if (systemTrayManager != null) {
+			systemTrayManager.message(message);
+		}
+	}
+
+	public void updateCurrent() {
+		current.setPomosDone(pomosDone);
+		current.setMaxPomosDone(settings.getPomosBeforeLongBreak());
+	}
 }
--- src/test/java/nl/ghyze/pomodoro/controller/PomodoroStateMachineTest.java
+++ src/test/java/nl/ghyze/pomodoro/controller/PomodoroStateMachineTest.java
@@ -1,40 +1,80 @@
 package nl.ghyze.pomodoro.controller;
 
+import java.lang.reflect.Field;
+
+import org.easymock.EasyMock;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import org.junit.Assert;
 import nl.ghyze.pomodoro.model.Pomodoro;
+import nl.ghyze.pomodoro.model.Pomodoro.Type;
 import nl.ghyze.pomodoro.model.Settings;
 
 public class PomodoroStateMachineTest {
 
-    private Settings settings;
-    private PomodoroStateMachine pomodoroStateMachine;
-    
-    @Before
-    public void setUp(){
-	createSettings();
-	pomodoroStateMachine = new PomodoroStateMachine(settings);
-    }
-    
-    private void createSettings(){
-	settings = new Settings();
-	settings.setPomoMinutes(1);
-	settings.setShortBreakMinutes(2);
-	settings.setLongBreakMinutes(3);
-	settings.setPomosBeforeLongBreak(4);
-	settings.setPosition(Settings.Position.TOP_LEFT);
-    }
-    
-    @Test
-    public void testGetCurrent(){
-	Pomodoro expected = new Pomodoro(0, Pomodoro.Type.WAIT);
-	Assert.assertEquals(expected, pomodoroStateMachine.getCurrent());
-    }
-    
-    @Test
-    public void testGetCurrentType(){
-	Assert.assertEquals(Pomodoro.Type.WAIT, pomodoroStateMachine.getCurrentType());
-    }
+	private Settings settings;
+	private PomodoroStateMachine pomodoroStateMachine;
+
+	@Before
+	public void setUp() {
+		settings = EasyMock.createMock(Settings.class);
+		pomodoroStateMachine = new PomodoroStateMachine(settings);
+	}
+
+	@Test
+	public void testGetCurrent() {
+		Pomodoro expected = new Pomodoro(0, Pomodoro.Type.WAIT);
+		Assert.assertEquals(expected, pomodoroStateMachine.getCurrent());
+	}
+
+	@Test
+	public void testGetCurrentType() {
+		Assert.assertEquals(Pomodoro.Type.WAIT, pomodoroStateMachine.getCurrentType());
+	}
+
+	@Test
+	public void testStartPomo() {
+		setupStartPomo();
+		EasyMock.replay(settings);
+		pomodoroStateMachine.startPomo();
+		EasyMock.verify(settings);
+
+		Pomodoro current = pomodoroStateMachine.getCurrent();
+		Assert.assertEquals(current.getType(), Type.POMO);
+
+	}
+	
+	private void setupStartPomo(){
+		EasyMock.expect(settings.getPomoMinutes()).andReturn(1);
+		EasyMock.expect(settings.getPomosBeforeLongBreak()).andReturn(1);
+	}
+
+	@Test
+	public void testShouldChangeState() throws Exception{
+		Assert.assertFalse(pomodoroStateMachine.shouldChangeState());
+		
+		Pomodoro pomodoroMock = EasyMock.createMock(Pomodoro.class);
+		EasyMock.expect(pomodoroMock.getType()).andReturn(Type.POMO);
+		EasyMock.expect(pomodoroMock.isDone()).andReturn(false);
+		setCurrent(pomodoroMock);
+		EasyMock.replay(pomodoroMock);
+		Assert.assertFalse(pomodoroStateMachine.shouldChangeState());
+		EasyMock.verify(pomodoroMock);
+		
+		EasyMock.reset(pomodoroMock);
+		
+		EasyMock.expect(pomodoroMock.getType()).andReturn(Type.POMO);
+		EasyMock.expect(pomodoroMock.isDone()).andReturn(true);
+		EasyMock.replay(pomodoroMock);
+		Assert.assertTrue(pomodoroStateMachine.shouldChangeState());
+		EasyMock.verify(pomodoroMock);
+
+	}
+	
+	private void setCurrent(Pomodoro current) throws Exception{
+		Field currentField = PomodoroStateMachine.class.getDeclaredField("current");
+		currentField.setAccessible(true);
+		currentField.set(pomodoroStateMachine, current);
+	}
 }
