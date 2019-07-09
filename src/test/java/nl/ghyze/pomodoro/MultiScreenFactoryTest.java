5f0d93058995970ebb16899909568287e80fc3ac
ghyze <gijsovervliet@studyportals.com>
2019-07-09 08:44:24 +0200

    Improved test

--- src/test/java/nl/ghyze/pomodoro/MultiScreenFactoryTest.java
+++ src/test/java/nl/ghyze/pomodoro/MultiScreenFactoryTest.java
@@ -2,22 +2,14 @@ package nl.ghyze.pomodoro;
 
 import static org.junit.Assert.assertEquals;
 
-import java.awt.*;
-import java.awt.datatransfer.Clipboard;
-import java.awt.dnd.DragGestureEvent;
-import java.awt.dnd.InvalidDnDOperationException;
-import java.awt.dnd.peer.DragSourceContextPeer;
-import java.awt.font.TextAttribute;
+import java.awt.DisplayMode;
+import java.awt.GraphicsConfiguration;
+import java.awt.GraphicsDevice;
+import java.awt.Point;
+import java.awt.Rectangle;
 import java.awt.geom.AffineTransform;
-import java.awt.im.InputMethodHighlight;
 import java.awt.image.ColorModel;
-import java.awt.image.ImageObserver;
-import java.awt.image.ImageProducer;
-import java.awt.peer.*;
-import java.net.URL;
 import java.util.List;
-import java.util.Map;
-import java.util.Properties;
 
 import nl.ghyze.pomodoro.model.Settings;
 import nl.ghyze.pomodoro.view.Screen;
@@ -28,16 +20,15 @@ import org.junit.Test;
 
 public class MultiScreenFactoryTest {
 
-	MultiScreenFactory factory;
-	GraphicsDevice graphicsDevice1;
-	GraphicsDevice graphicsDevice2;
-	GraphicsDevice[] devices;
-	
+	private MultiScreenFactory factory;
+	private GraphicsDevice graphicsDevice1;
+	private GraphicsDevice graphicsDevice2;
+
 	@Before
 	public void setup(){
 		graphicsDevice1 = createGraphicsDeviceMock(0, 0, 800, 600);
 		graphicsDevice2 = createGraphicsDeviceMock(800, 150, 1024, 768);
-		devices = new GraphicsDevice[]{graphicsDevice1, graphicsDevice2};
+		GraphicsDevice[] devices = new GraphicsDevice[]{graphicsDevice1, graphicsDevice2};
 		factory = new MultiScreenFactory();
 		factory.setAvailableGraphicsDevices(devices);
 		factory.setToolkit(new TestToolkit());
@@ -90,7 +81,7 @@ public class MultiScreenFactoryTest {
 		DisplayMode displayMode = new DisplayMode(width, height, 24, 60);
 		EasyMock.expect(graphicsDevice.getDisplayMode()).andReturn(displayMode);
 		
-		MockGraphicsCofiguration configuration = new MockGraphicsCofiguration();	
+		MockGraphicsConfiguration configuration = new MockGraphicsConfiguration();
 		configuration.bounds = new Rectangle(x, y, width, height);
 		EasyMock.expect(graphicsDevice.getDefaultConfiguration()).andReturn(configuration).anyTimes();
 		
@@ -98,9 +89,9 @@ public class MultiScreenFactoryTest {
 		return graphicsDevice;
 	}
 	
-	class MockGraphicsCofiguration extends GraphicsConfiguration{
+	class MockGraphicsConfiguration extends GraphicsConfiguration{
 
-		public Rectangle bounds;
+		Rectangle bounds;
 		
 		@Override
 		public GraphicsDevice getDevice() {
