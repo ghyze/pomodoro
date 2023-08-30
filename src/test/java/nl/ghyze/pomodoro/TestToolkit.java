86bca80ab1b7453f6f1c217eedebacddab23bca9
Gijs Overvliet <gijs.overvliet@rabobank.nl>
2023-08-30 08:57:06 +0200

    Updated versions

--- src/test/java/nl/ghyze/pomodoro/TestToolkit.java
+++ src/test/java/nl/ghyze/pomodoro/TestToolkit.java
@@ -2,129 +2,16 @@ package nl.ghyze.pomodoro;
 
 import java.awt.*;
 import java.awt.datatransfer.Clipboard;
-import java.awt.dnd.DragGestureEvent;
-import java.awt.dnd.InvalidDnDOperationException;
-import java.awt.dnd.peer.DragSourceContextPeer;
 import java.awt.font.TextAttribute;
 import java.awt.im.InputMethodHighlight;
 import java.awt.image.ColorModel;
 import java.awt.image.ImageObserver;
 import java.awt.image.ImageProducer;
-import java.awt.peer.*;
 import java.net.URL;
 import java.util.Map;
 import java.util.Properties;
 
 public class TestToolkit extends Toolkit {
-    @Override
-    protected DesktopPeer createDesktopPeer(Desktop target) throws HeadlessException {
-        return null;
-    }
-
-    @Override
-    protected ButtonPeer createButton(Button target) throws HeadlessException {
-        return null;
-    }
-
-    @Override
-    protected TextFieldPeer createTextField(TextField target) throws HeadlessException {
-        return null;
-    }
-
-    @Override
-    protected LabelPeer createLabel(Label target) throws HeadlessException {
-        return null;
-    }
-
-    @Override
-    protected ListPeer createList(List target) throws HeadlessException {
-        return null;
-    }
-
-    @Override
-    protected CheckboxPeer createCheckbox(Checkbox target) throws HeadlessException {
-        return null;
-    }
-
-    @Override
-    protected ScrollbarPeer createScrollbar(Scrollbar target) throws HeadlessException {
-        return null;
-    }
-
-    @Override
-    protected ScrollPanePeer createScrollPane(ScrollPane target) throws HeadlessException {
-        return null;
-    }
-
-    @Override
-    protected TextAreaPeer createTextArea(TextArea target) throws HeadlessException {
-        return null;
-    }
-
-    @Override
-    protected ChoicePeer createChoice(Choice target) throws HeadlessException {
-        return null;
-    }
-
-    @Override
-    protected FramePeer createFrame(Frame target) throws HeadlessException {
-        return null;
-    }
-
-    @Override
-    protected CanvasPeer createCanvas(Canvas target) {
-        return null;
-    }
-
-    @Override
-    protected PanelPeer createPanel(Panel target) {
-        return null;
-    }
-
-    @Override
-    protected WindowPeer createWindow(Window target) throws HeadlessException {
-        return null;
-    }
-
-    @Override
-    protected DialogPeer createDialog(Dialog target) throws HeadlessException {
-        return null;
-    }
-
-    @Override
-    protected MenuBarPeer createMenuBar(MenuBar target) throws HeadlessException {
-        return null;
-    }
-
-    @Override
-    protected MenuPeer createMenu(Menu target) throws HeadlessException {
-        return null;
-    }
-
-    @Override
-    protected PopupMenuPeer createPopupMenu(PopupMenu target) throws HeadlessException {
-        return null;
-    }
-
-    @Override
-    protected MenuItemPeer createMenuItem(MenuItem target) throws HeadlessException {
-        return null;
-    }
-
-    @Override
-    protected FileDialogPeer createFileDialog(FileDialog target) throws HeadlessException {
-        return null;
-    }
-
-    @Override
-    protected CheckboxMenuItemPeer createCheckboxMenuItem(CheckboxMenuItem target) throws HeadlessException {
-        return null;
-    }
-
-    @Override
-    protected FontPeer getFontPeer(String name, int style) {
-        return null;
-    }
 
     @Override
     public Dimension getScreenSize() throws HeadlessException {
@@ -213,11 +100,6 @@ public class TestToolkit extends Toolkit {
 
     @Override
     protected EventQueue getSystemEventQueueImpl() {
-        return null;
-    }
-
-    @Override
-    public DragSourceContextPeer createDragSourceContextPeer(DragGestureEvent dge) throws InvalidDnDOperationException {
         return null;
     }
 
